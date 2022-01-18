d085a0b23b9167079c8eb718cff46b077e7bc062
Christina Song <christina.song202@gmail.com>
2022-01-18 01:10:50 -0500

    fixed db connection problem

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
 const dbURI =
-  "mongodb+srv://csong202:<1Mongo>@cluster0.ty41r.mongodb.net/shopify_db?retryWrites=true&w=majority";
+  "mongodb+srv://csong202:<mongoDB123>@cluster0.ty41r.mongodb.net/shopify_db?retryWrites=true&w=majority";
 
 mongoose
-  .connect(dbURI, { useNewUrlParser: true, userUnifiedTopology: true })
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    auth: { username: "csong202", password: "mongoDB123" },
+  })
   .then(function (result) {
     console.log("Database is connected");
   })
