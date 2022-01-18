7d6e1367f1b169e50d40a5d2614cb2631242c019
Christina Song <christina.song202@gmail.com>
2022-01-18 00:41:36 -0500

    setup mongodb

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("testing testing");
+const mongoose = require("mongoose");
+
+const dbURI =
+  "mongodb+srv://csong202:<1Mongo>@cluster0.ty41r.mongodb.net/shopify_db?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, userUnifiedTopology: true })
+  .then(function (result) {
+    console.log("Database is connected");
+  })
+  .catch((err) => console.log(err));
